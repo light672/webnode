2a102397692c06393832f42e58054c3cfc3b0357
light672 <lightdevelopment6@gmail.com>
2023-10-01 20:06:55 -0400

    Add component based system with logging to output (try not to leak personal information next time)

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
-
-
+#include "Component.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Component root = Component("body");
+    Component child0 = Component("img");
+    Component child1 = Component("h3");
+    Component child1Child0 = Component("Caption");
+    Component child2 = Component("p");
+    Component child2Child0 = Component("Sub Caption");
+    root.addChild(&child0);
+    root.addChild(&child1);
+    root.addChild(&child2);
+
+    child1.addChild(&child1Child0);
+    child2.addChild(&child2Child0);
+    root.printHierarchy("body");
     return 0;
 }
